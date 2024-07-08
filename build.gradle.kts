6c67cef450344eb1166cab970f8d67679c0fc61c
Riccardo Battistini <riccardo.battistini@protonmail.com>
2024-07-08 11:30:40 +0200

    Update linter

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     scala
     application
     id("org.openjfx.javafxplugin") version "0.0.13"
-    id("com.github.eugenesy.scapegoat") version "0.2.0"
+    id("io.github.cosmicsilence.scalafix") version "0.2.2"
 }
 
 repositories {
@@ -15,21 +15,6 @@ dependencies {
     implementation("org.scalafx:scalafx_3:21.0.0-R32")
     implementation("io.monix:monix_3:3.4.1")
     implementation("org.jfree:jfreechart:1.5.3")
-}
-
-configure<com.github.eugenesy.scapegoat.ScapegoatExtension>  {
-    scapegoatVersion = "1.4.8"
-    scalaVersion = "2.12.13"
-    dataDir = "${buildDir}/reports/scapegoat"
-    disabledInspections = arrayListOf("ArrayEquals", "AvoidToMinusOne")
-    ignoredFiles = emptyArray<String>().toList()
-    consoleOutput = true
-    verbose = true
-    reports = arrayListOf<String>("html", "xml")
-    sourcePrefix = "src/main/scala"
-    minimalWarnLevel = "info"
-    enable = true
-    testEnable = true
 }
 
 javafx {
