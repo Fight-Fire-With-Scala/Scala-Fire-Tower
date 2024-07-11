eac7b096761a5694a52cfa03a3d68cac590ab383
Riccardo Battistini <riccardo.battistini@protonmail.com>
2024-07-11 12:39:44 +0200

    refactor: use logger instead of println

--- app/src/main/scala/it/unibo/model/Cell.scala
+++ app/src/main/scala/it/unibo/model/Cell.scala
@@ -1,11 +1,19 @@
 package it.unibo.model
 
+import com.typesafe.scalalogging.Logger
+
+val logger = Logger("name")
+
 sealed trait CellType {
   def name: String
 }
 
 case object EternalFire extends CellType {
   val name = "Eternal Fire"
+}
+
+case object Woods extends CellType {
+  val name = "Woods"
 }
 
 sealed trait CellState {
@@ -18,10 +26,9 @@ case object Firebreaker extends CellState {
 
 case class Cell(cellType: CellType, cellStatus: CellState)
 
-def parseCellState(state: CellState): Unit =
-  state match
-    case Firebreaker => println(s"This is a cell in state ${Firebreaker.name}")
+def parseCellState(state: CellState): Unit = state match
+  case Firebreaker => logger.debug(s"This is a cell in state ${Firebreaker.name}")
 
-def parseCellType(cellType: CellType): Unit =
-  cellType match
-    case EternalFire => println(s"This is a cell of type ${EternalFire.name}")
+def parseCellType(cellType: CellType): Unit = cellType match
+  case EternalFire => logger.debug(s"This is a cell of type ${EternalFire.name}")
+  case Woods => logger.debug(s"This is a cell of type ${Woods.name}")
