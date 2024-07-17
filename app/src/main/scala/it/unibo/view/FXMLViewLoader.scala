ca04bc64f5e6d9089a82c0342af4e8407119e7fa
Tonelli Luca <luca.tonelli11@studio.unibo.it>
2024-07-17 23:48:52 +0200

    refactor: changed the controller integration in the view

--- app/src/main/scala/it/unibo/view/FXMLViewLoader.scala
+++ app/src/main/scala/it/unibo/view/FXMLViewLoader.scala
@@ -1,17 +1,22 @@
 package it.unibo.view
 
+import it.unibo.view.controllers.utils.{Notifier, StartGameNotifier}
+import it.unibo.view.controllers.menu.{MenuController, MenuControllerService}
 import javafx.fxml.FXMLLoader
 import javafx.scene.Parent
 
-trait ViewLoader {
-  def load[T](fxmlPath: String): (Parent, T)
-}
+trait ViewLoader:
+  def load(fxmlPath: String): (Parent, MenuControllerService)
 
-class FXMLViewLoader extends ViewLoader {
-  override def load[T](fxmlPath: String): (Parent, T) = {
+class FXMLViewLoader extends ViewLoader:
+  override def load(fxmlPath: String): (Parent, MenuControllerService) =
     val loader = new FXMLLoader(getClass.getResource(fxmlPath))
+    val notifier = new StartGameNotifier()
+
+    val actualController: MenuControllerService = new MenuController(notifier)
+    loader.setController(actualController)
+
+    //    val proxyController = new MenuControllerProxy(actualController)
     val root: Parent = loader.load()
-    val controller: T = loader.getController
-    (root, controller)
-  }
-}
+
+    (root, actualController)
