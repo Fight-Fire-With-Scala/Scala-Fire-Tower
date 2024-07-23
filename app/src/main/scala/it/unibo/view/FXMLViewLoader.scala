ba2c1b7ad8a5a84e73f48c3e842ac445a3b82e59
Tonelli Luca <luca.tonelli11@studio.unibo.it>
2024-07-23 18:06:46 +0200

    refactor: generalized the view loader for enhanced flexibility

--- app/src/main/scala/it/unibo/view/FXMLViewLoader.scala
+++ app/src/main/scala/it/unibo/view/FXMLViewLoader.scala
@@ -6,17 +6,12 @@ import javafx.fxml.FXMLLoader
 import javafx.scene.Parent
 
 trait ViewLoader:
-  def load(fxmlPath: String): (Parent, MenuControllerService)
+  def load[T](fxmlPath: String, controller: T): Parent
 
 class FXMLViewLoader extends ViewLoader:
-  override def load(fxmlPath: String): (Parent, MenuControllerService) =
+  override def load[T](fxmlPath: String, controller: T): Parent =
     val loader = new FXMLLoader(getClass.getResource(fxmlPath))
-    val notifier = new StartGameNotifier()
-
-    val actualController: MenuControllerService = new MenuController(notifier)
-    loader.setController(actualController)
-
+    loader.setController(controller)
     //    val proxyController = new MenuControllerProxy(actualController)
     val root: Parent = loader.load()
-
-    (root, actualController)
+    root
--- app/src/main/scala/it/unibo/view/MonadicGuiFX.scala
+++ app/src/main/scala/it/unibo/view/MonadicGuiFX.scala
@@ -1,10 +1,14 @@
 package it.unibo.view
 
 import it.unibo.controller.ControllerModule
+import it.unibo.view.controllers.GraphicController
+import it.unibo.view.controllers.hand.HandController
+import it.unibo.view.controllers.hand.cards.CardController
+import it.unibo.view.controllers.menu.MenuController
+import it.unibo.view.controllers.utils.StartGameNotifier
 import monix.eval.Task
 import monix.execution.Scheduler.Implicits.global
-import javafx.fxml.FXMLLoader
-import javafx.scene.Parent
+import javafx.scene.Node
 import scalafx.application.{JFXApp3, Platform}
 import scalafx.application.JFXApp3.PrimaryStage
 import scalafx.scene.Scene
@@ -12,44 +16,47 @@ import scalafx.scene.layout.Pane
 
 import scala.compiletime.uninitialized
 
-object GUIType extends Enumeration {
-  val Menu: Value = Value
+object GUIType extends Enumeration:
+  type GUIType = Value
+  val Menu: GUIType.Value = Value
+  val Hand: GUIType.Value = Value
+  val Card: GUIType.Value = Value
 
-  def fxmlPath(value: GUIType.Value): String = value match { case Menu => "/pages/menu.fxml" }
-}
+  def fxmlPathAndController(
+      value: GUIType.Value,
+      controller: ControllerModule.Controller
+  ): (String, GraphicController) = value match
+    case Menu =>
+      val notifier = new StartGameNotifier(controller.notifyHand())
+      ("/pages/menu.fxml", new MenuController(notifier))
+    case Hand => ("/pages/hand.fxml", new HandController())
+    case Card => ("/pages/card.fxml", new CardController())
 
 class MonadicGuiFX(
     val w: Int,
     val h: Int,
     controller: ControllerModule.Controller,
     viewLoader: ViewLoader
-) extends JFXApp3 {
+) extends JFXApp3:
 
   private var pane: Pane = uninitialized
 
-  override def start(): Unit = {
+  override def start(): Unit =
     pane = new Pane()
-    stage = new PrimaryStage {
+    stage = new PrimaryStage:
       scene = new Scene(pane, w, h)
       minHeight = 600
       minWidth = 900
-    }
     loadGUI(GUIType.Menu)
-  }
 
-  import scalafx.Includes._
-  import scalafx.scene.Node
-
-  def loadGUI(guiType: GUIType.Value): Unit = {
-    val fxmlPath = GUIType.fxmlPath(guiType)
+  def loadGUI(guiType: GUIType.Value): Unit =
+    val (fxmlPath, controllerInstance) = GUIType.fxmlPathAndController(guiType, controller)
     val guiTask = Task {
-      val (root, _) = viewLoader.load(fxmlPath)
+      val root = viewLoader.load(fxmlPath, controllerInstance)
       Platform.runLater { () =>
         pane.children.clear()
-        pane.children.add(root)
+        pane.children.add(root.asInstanceOf[Node])
         stage.show()
       }
     }
     guiTask.runAsyncAndForget
-  }
-}
