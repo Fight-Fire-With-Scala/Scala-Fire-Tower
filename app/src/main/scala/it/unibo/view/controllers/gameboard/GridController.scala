9f33b2421c40b8bed7e1cb5d1f4dacda24b7e2f2
Alessandro Becci <alessandro.becci@studio.unibo.it>
2024-07-24 17:06:47 +0200

    feat: improved grid view and added click function to put text inside as a testing purpose

--- app/src/main/scala/it/unibo/view/controllers/gameboard/GridController.scala
+++ app/src/main/scala/it/unibo/view/controllers/gameboard/GridController.scala
@@ -6,20 +6,23 @@ import javafx.scene.layout.{GridPane, Pane}
 import javafx.scene.shape.Rectangle
 import javafx.application.Platform
 import javafx.scene.paint.Color
+import javafx.scene.input.MouseEvent
+import javafx.scene.text.{Font, Text}
 
-import scala.compiletime.uninitialized
+import scala.collection.mutable
 
 class GridController extends GraphicController {
 
   @FXML
-  private var gridPane: GridPane = uninitialized
+  private var gridPane: GridPane = _
   private val gridSize = 16
+  private val rectangleMap: mutable.Map[(Int, Int), Rectangle] = mutable.Map()
 
   @FXML
   def initialize(): Unit = {
     // Set fixed size constraints for the GridPane
     gridPane.setMinSize(900, 600)
-    //gridPane.setMaxSize(900, 600)
+    gridPane.setMaxSize(900, 600)
 
     // Dynamically create and add panes with rectangles to the GridPane
     for (i <- 0 until gridSize; j <- 0 until gridSize) {
@@ -27,9 +30,13 @@ class GridController extends GraphicController {
       val rectangle = new Rectangle()
       rectangle.setStroke(Color.BLACK)
       rectangle.setStrokeWidth(1)
-      rectangle.setFill(Color.LIGHTGRAY)
+      rectangle.setFill(Color.DARKCYAN)
+      rectangle.addEventHandler(MouseEvent.MOUSE_CLICKED, (_: MouseEvent) => {
+        handleRectangleClick(i, j)
+      })
       pane.getChildren.add(rectangle)
       gridPane.add(pane, i, j)
+      rectangleMap((i, j)) = rectangle
     }
 
     // Ensure we run updateRectangleSizes after the layout is complete
@@ -50,13 +57,24 @@ class GridController extends GraphicController {
 
     // Calculate the minimum dimension to ensure cells are square
     val squareSize = if(width > 0) { width / numColumns } else { 0 }
-
+    println("Resize event: width=" + width + ", height=" + height + ", squareSize=" + squareSize)
     // Resize rectangles within panes to be square and fill the entire cell
     for (i <- 0 until numColumns; j <- 0 until numRows) {
-      val pane = gridPane.getChildren.get(i * numColumns + j).asInstanceOf[Pane]
-      val rectangle = pane.getChildren.get(0).asInstanceOf[Rectangle]
+      val rectangle = rectangleMap((i, j))
       rectangle.setWidth(squareSize)
       rectangle.setHeight(squareSize)
     }
+  }
+
+  private def handleRectangleClick(row: Int, col: Int): Unit = {
+    val rectangle = rectangleMap((row, col))
+    val pane = rectangle.getParent.asInstanceOf[Pane]
+    val text = new Text("!")
+    text.setFill(Color.RED)
+    text.setFont(new Font(rectangle.getHeight / 2)) // Adjust font size to fit within the rectangle
+    text.setX(rectangle.getWidth / 2 - text.getLayoutBounds.getWidth / 2)
+    text.setY(rectangle.getHeight / 2 + text.getLayoutBounds.getHeight / 6)
+    pane.getChildren.add(text)
+    println(s"Rectangle at ($row, $col) clicked")
   }
 }
