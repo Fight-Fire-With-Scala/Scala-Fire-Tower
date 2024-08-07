88fbc225c38037b6aa34908cded99cd7aeee7cb8
Alessandro Becci <alessandro.becci@studio.unibo.it>
2024-08-07 15:44:32 +0200

    refactor: given HoverDirection another file

--- app/src/main/scala/it/unibo/view/controllers/gameboard/GridSquare.scala
+++ app/src/main/scala/it/unibo/view/controllers/gameboard/GridSquare.scala
@@ -8,18 +8,12 @@ import javafx.util.Duration
 
 import scala.compiletime.uninitialized
 
-enum HoverDirection:
-  case North, South, West, East, NotDetermined
-
-object HoverDirection:
-  def fromCoordinates(x: Double, y: Double, width: Double, height: Double): HoverDirection =
-    if y < height / 3 && x >= width / 3 && x <= 2 * width / 3 then HoverDirection.North
-    else if y > 2 * height / 3 && x >= width / 3 && x <= 2 * width / 3 then HoverDirection.South
-    else if x < width / 3 && y >= height / 3 && y <= 2 * height / 3 then HoverDirection.West
-    else if x > 2 * width / 3 && y >= height / 3 && y <= 2 * height / 3 then HoverDirection.East
-    else HoverDirection.NotDetermined
-
-case class GridSquare(row: Int, col: Int, size: Double, onHover: (Int, Int, HoverDirection) => Unit):
+case class GridSquare(
+    row: Int,
+    col: Int,
+    size: Double,
+    onHover: (Int, Int, HoverDirection) => Unit
+):
   private val hoverDelayMillis = 250
 
   private val rectangle: Rectangle = new Rectangle:
