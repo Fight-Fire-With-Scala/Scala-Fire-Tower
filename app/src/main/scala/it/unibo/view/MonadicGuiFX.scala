c5871179920765d3988fdc570b284b15c2e1d167
Tonelli Luca <luca.tonelli11@studio.unibo.it>
2024-08-08 19:09:35 +0200

    refactor: add dynamic card slot load

--- app/src/main/scala/it/unibo/view/MonadicGuiFX.scala
+++ app/src/main/scala/it/unibo/view/MonadicGuiFX.scala
@@ -1,20 +1,19 @@
 package it.unibo.view
 
-import it.unibo.controller.ControllerModule
 import monix.eval.Task
 import monix.execution.Scheduler.Implicits.global
 import javafx.scene.Node
 import scalafx.application.{JFXApp3, Platform}
 import scalafx.application.JFXApp3.PrimaryStage
 import scalafx.scene.Scene
-import scalafx.scene.layout.Pane
+import scalafx.scene.layout.{Pane, StackPane}
 import it.unibo.view.components.{ComponentFactory, GraphicComponent, GraphicComponentRegistry}
 import it.unibo.view.components.hand.HandComponent
 import it.unibo.view.components.hand.cards.CardComponent
-import monix.reactive.subjects.PublishSubject
 import it.unibo.controller.ViewSubject
 
 import scala.compiletime.uninitialized
+import scala.jdk.CollectionConverters.*
 
 class MonadicGuiFX(val w: Int, val h: Int, viewLoader: ViewLoader, observableSubject: ViewSubject)
     extends JFXApp3:
@@ -22,22 +21,19 @@ class MonadicGuiFX(val w: Int, val h: Int, viewLoader: ViewLoader, observableSub
   private var pane: Pane = uninitialized
 
   override def start(): Unit =
-    pane = new Pane()
+    pane = new StackPane()
     stage = new PrimaryStage:
       scene = new Scene(pane, w, h)
       minHeight = 720
       minWidth = 1280
-    loadGUI(GUIType.Menu)
+    loadHand()
 
-  private def loadGuiComponent(
-      guiType: GUIType,
-      setup: Option[GraphicComponent => Unit] = None
-  ): Unit =
+  private def loadGuiComponent(guiType: GUIType, setups: (GraphicComponent => Unit)*): Unit =
     val guiTask = Task {
       val componentInstance = ComponentFactory.createFXMLComponent(guiType)(observableSubject)
       GraphicComponentRegistry.register(componentInstance)
       val root = viewLoader.load(guiType.fxmlPath, componentInstance).asInstanceOf[Node]
-      setup.foreach(_(componentInstance))
+      setups.foreach(_(componentInstance))
       Platform.runLater { () =>
         pane.children.clear()
         pane.children.add(root)
@@ -52,13 +48,12 @@ class MonadicGuiFX(val w: Int, val h: Int, viewLoader: ViewLoader, observableSub
 
   def loadHand(): Unit = loadGuiComponent(
     GUIType.Hand,
-    Some { graphicComponent =>
+    graphicComponent =>
       val handComponent = graphicComponent.asInstanceOf[HandComponent]
-      (0 to 4).foreach { slotIndex =>
+      handComponent.handPane.getChildren.asScala.zipWithIndex.foreach { case (_, slotIndex) =>
         val cardComponent = ComponentFactory.createFXMLComponent(GUIType.Card)(observableSubject)
           .asInstanceOf[CardComponent]
         val cardView = viewLoader.load(GUIType.Card.fxmlPath, cardComponent).asInstanceOf[Node]
-        handComponent.setupCard(cardView, cardComponent, slotIndex)
-      }
-    }
+        handComponent.setupCard(cardView, cardComponent, slotIndex)      }
+
   )
--- app/src/main/scala/it/unibo/view/components/hand/HandComponent.scala
+++ app/src/main/scala/it/unibo/view/components/hand/HandComponent.scala
@@ -8,39 +8,28 @@ import scalafx.application.Platform
 import javafx.scene.Node
 
 import scala.compiletime.uninitialized
+import scala.jdk.CollectionConverters.*
 
 class HandComponent extends GraphicComponent:
   @FXML
   var handPane: Pane = uninitialized
-  @FXML
-  var slot0: Pane = uninitialized
-  @FXML
-  var slot1: Pane = uninitialized
-  @FXML
-  var slot2: Pane = uninitialized
-  @FXML
-  var slot3: Pane = uninitialized
-  @FXML
-  var slot4: Pane = uninitialized
 
-  private var cardComponents: List[Option[CardComponent]] = List.fill(5)(None)
+  private var cardComponents: List[Option[CardComponent]] = List.empty
 
-  def setupCard(cardPane: Node, cardComponent: CardComponent, slotIndex: Int): Unit = Platform
-    .runLater { () =>
-      val slot = slotIndex match
-        case 0 => slot0
-        case 1 => slot1
-        case 2 => slot2
-        case 3 => slot3
-        case 4 => slot4
-      slot.getChildren.clear()
-      val pane = cardPane.asInstanceOf[Pane]
-      pane.layoutXProperty().bind(slot.widthProperty().subtract(pane.widthProperty()).divide(2))
-      pane.layoutYProperty().bind(slot.heightProperty().subtract(pane.heightProperty()).divide(2))
-      pane.getStyleClass.add("default")
-      slot.getChildren.add(pane)
-      cardComponents = cardComponents.updated(slotIndex, Some(cardComponent))
-    }
+  @FXML
+  def initialize(): Unit =
+    cardComponents = handPane.getChildren.asScala.toList.map(_ => None)
+
+  def setupCard(cardPane: Node, cardComponent: CardComponent, slotIndex: Int): Unit = Platform.runLater { () =>
+    val slot = handPane.getChildren.get(slotIndex).asInstanceOf[Pane]
+    slot.getChildren.clear()
+    val pane = cardPane.asInstanceOf[Pane]
+    pane.layoutXProperty().bind(slot.widthProperty().subtract(pane.widthProperty()).divide(2))
+    pane.layoutYProperty().bind(slot.heightProperty().subtract(pane.heightProperty()).divide(2))
+    pane.getStyleClass.add("default")
+    slot.getChildren.add(pane)
+    cardComponents = cardComponents.updated(slotIndex, Some(cardComponent))
+  }
 
   def updateHand(cards: List[it.unibo.model.cards.Card]): Unit = Platform.runLater { () =>
     cards.zipWithIndex.foreach { case (card, index) =>
