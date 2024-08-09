be6aa47d12c0f292531c06a428cd57d2453e318e
Tonelli Luca <luca.tonelli11@studio.unibo.it>
2024-08-09 12:03:24 +0200

    feat: implement Game Board controller

---
+++ app/src/main/scala/it/unibo/view/GameBoardController.scala
@@ -0,0 +1,13 @@
+package it.unibo.view
+
+import it.unibo.model.gameboard.grid.Grid
+import it.unibo.view.components.game.GameComponent
+
+object GameBoardController:
+  private var gameComponent: Option[GameComponent] = None
+
+  def initialize(component: GameComponent): Unit =
+    gameComponent = Some(component)
+
+  def refresh(grid: Grid): Unit =
+    gameComponent.foreach(_.updateGrid(grid))
