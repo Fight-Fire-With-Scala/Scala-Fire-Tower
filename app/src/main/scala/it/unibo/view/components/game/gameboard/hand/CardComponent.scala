2bab63cd0c8d4c5b3e7c7727fb2bc31002675d55
Tonelli Luca <luca.tonelli11@studio.unibo.it>
2024-08-17 19:09:53 +0200

    refactor: add missed trait

--- app/src/main/scala/it/unibo/view/components/game/gameboard/hand/CardComponent.scala
+++ app/src/main/scala/it/unibo/view/components/game/gameboard/hand/CardComponent.scala
@@ -3,8 +3,7 @@ package it.unibo.view.components.game.gameboard.hand
 import it.unibo.model.cards.Card.allCards
 import it.unibo.model.cards.types.{FireCard, FirebreakCard, WaterCard, WindCard}
 import it.unibo.model.cards.{Card, CardType}
-import it.unibo.view.components.GraphicComponent
-import it.unibo.view.components.IHaveView
+import it.unibo.view.components.{GraphicComponent, ICanBeDisabled, IHaveView}
 import javafx.event.EventHandler
 import javafx.fxml.FXML
 import javafx.scene.input.MouseEvent
@@ -15,7 +14,7 @@ import scalafx.Includes.*
 import scala.compiletime.uninitialized
 
 //noinspection VarCouldBeVal
-final class CardComponent extends GraphicComponent with IHaveView:
+final class CardComponent extends IHaveView with ICanBeDisabled:
 
   override val fxmlPath: String = "/pages/card.fxml"
 
