d2a14837b83fe222ff0abc8ae9b41822c03f10d2
Tonelli Luca <luca.tonelli11@studio.unibo.it>
2024-09-03 02:25:47 +0200

    refactor: simplify trait usage

--- app/src/main/scala/it/unibo/view/components/ICanToggleHandler.scala
+++ app/src/main/scala/it/unibo/view/components/ICanToggleHandler.scala
@@ -1,6 +1,5 @@
 package it.unibo.view.components
 
-import it.unibo.view.logger
 import javafx.event.{EventHandler, EventType}
 import javafx.scene.input.MouseEvent
 
@@ -16,7 +15,23 @@ trait ICanToggleHandler[T] extends Toggleable[T]:
 
   protected def applyState(state: T): Unit
 
-  protected def onToggle(state: T): Unit
+  private def updateHandlers(
+      state: T,
+      action: (EventType[MouseEvent], EventHandler[MouseEvent]) => Unit
+  ): Unit = stateHandlers.get(state).foreach { eventHandlers =>
+    eventHandlers.foreach { case (eventType, handlers) =>
+      handlers.foreach(handler => action(eventType, handler))
+    }
+  }
+
+  protected def disableActualHandlers(): Unit =
+    updateHandlers(currentState, getPane.removeEventHandler)
+
+  protected def enableActualHandlers(): Unit = updateHandlers(currentState, getPane.addEventHandler)
+
+  protected def onToggle(state: T): Unit =
+    disableActualHandlers()
+    updateHandlers(state, getPane.addEventHandler)
 
   def addHandler(
       state: T,
@@ -26,6 +41,7 @@ trait ICanToggleHandler[T] extends Toggleable[T]:
     val eventHandlers = stateHandlers.getOrElse(state, Map())
     val handlers = eventHandlers.getOrElse(eventType, List())
     stateHandlers += (state -> (eventHandlers + (eventType -> (handler :: handlers))))
+
 
   def removeHandler(
       state: T,
@@ -41,8 +57,9 @@ trait ICanToggleHandler[T] extends Toggleable[T]:
 
   override def toggle(toState: T): Unit =
     currentState = toState
-    //logger.info(s"Current state $currentState")
     applyState(toState)
     onToggle(toState)
 
   override def getCurrentState: T = currentState
+
+  protected def getPane: javafx.scene.Node
