10de7de9cd3ab870c1c636d34bc0424bf0a3b5e6
Alessandro Becci <alessandro.becci@studio.unibo.it>
2024-09-06 18:10:28 +0200

    feat: adapted Grid for not placing fire on firebreak cells

--- app/src/main/scala/it/unibo/model/gameboard/grid/Grid.scala
+++ app/src/main/scala/it/unibo/model/gameboard/grid/Grid.scala
@@ -71,16 +71,24 @@ final case class BasicGrid(
 
   override def setToken(position: Position, token: Token): Grid = getCell(position) match
     case Some(_: EternalFire.type) => this
-    case Some(_: Woods.type)       => getToken(position) match
-        case Some(Fire)                                                   =>
-          if token == Water then BasicGrid(this._cells, this._tokens - position)
-          else BasicGrid(this._cells, this._tokens + (position -> token))
-        case Some(Firebreak) | Some(Water) | Some(Reforest) | Some(Empty) =>
-          BasicGrid(this._cells, this._tokens + (position -> token))
-        case _                                                            =>
-          if token == Water || token == Empty then this
-          else BasicGrid(this._cells, this._tokens + (position -> token))
+    case Some(_: Woods.type)       => handleTokenForWoods(position, token)
     case _                         => BasicGrid(this._cells, this._tokens + (position -> token))
+
+  private def handleTokenForWoods(position: Position, token: Token): Grid =
+    getToken(position) match
+      case Some(Fire) =>
+        token match
+          case Water => BasicGrid(this._cells, this._tokens - position)
+          case _ => BasicGrid(this._cells, this._tokens + (position -> token))
+      case Some(Firebreak)   =>
+        token match
+          case Fire => this
+          case _ => BasicGrid(this._cells, this._tokens + (position -> token))
+      case Some(Water) | Some(Reforest) | Some(Empty) =>
+        BasicGrid(this._cells, this._tokens + (position -> token))
+      case _ =>
+        if token == Water || token == Empty then this
+        else BasicGrid(this._cells, this._tokens + (position -> token))
 
   override def getToken(position: Position): Option[Token] = this._tokens.get(position)
 
