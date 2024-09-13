71c7f65b8c18722585e0013eb6d3e6bade47ccf7
Riccardo Battistini <riccardo.battistini@protonmail.com>
2024-09-13 15:40:43 +0200

    refactor: make the phase manager work with a bot too

--- app/src/main/scala/it/unibo/model/effect/phase/PhaseManager.scala
+++ app/src/main/scala/it/unibo/model/effect/phase/PhaseManager.scala
@@ -1,23 +1,27 @@
 package it.unibo.model.effect.phase
 
 import scala.annotation.tailrec
-
 import it.unibo.model.effect.GameBoardEffect
 import it.unibo.model.effect.card.WindEffect
 import it.unibo.model.effect.card.WindEffect.given_Conversion_Direction_WindEffect
 import it.unibo.model.effect.pattern.PatternEffect
 import it.unibo.model.gameboard.GameBoard
 import it.unibo.model.gameboard.GamePhase
-import it.unibo.model.gameboard.GamePhase._
+import it.unibo.model.gameboard.GamePhase.*
+import it.unibo.model.gameboard.player.{Person, Player}
 
 trait PhaseManager:
   @tailrec
   final def updateGamePhase(gb: GameBoard, choice: GamePhase): GameBoard = choice match
-    case WindPhase             => handleWindPhase(gb)
+    case WindPhase             => gb.getCurrentPlayer match
+        case p: Person => handleWindPhase(gb)
+        case _         => gb
     case WaitingPhase          => gb.copy(gamePhase = WaitingPhase)
     case PlayStandardCardPhase => gb.copy(gamePhase = PlayStandardCardPhase)
     case RedrawCardsPhase      => gb.copy(gamePhase = RedrawCardsPhase)
-    case DecisionPhase         => updateGamePhase(gb, getNextPhaseAfterDecisionPhase(gb))
+    case DecisionPhase         => gb.getCurrentPlayer match
+        case p: Person => updateGamePhase(gb, getNextPhaseAfterDecisionPhase(gb))
+        case _         => gb
     case PlaySpecialCardPhase  => gb.copy(gamePhase = PlaySpecialCardPhase)
     case EndTurnPhase          => updateGamePhase(handleTurnEnd(gb), WindPhase)
 
