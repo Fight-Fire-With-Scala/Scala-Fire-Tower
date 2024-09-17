940765144dfa7486e209a9754c652dfa417bf57f
Riccardo Battistini <riccardo.battistini@protonmail.com>
2024-09-17 19:07:40 +0200

    test: update fix for gameboard init

--- src/test/scala/it/unibo/model/prolog/decisionmaking/PatternChoiceSpec.scala
+++ src/test/scala/it/unibo/model/prolog/decisionmaking/PatternChoiceSpec.scala
@@ -6,10 +6,10 @@ import it.unibo.model.effect.pattern.LogicSolverManager
 import it.unibo.model.effect.pattern.PatternEffect.BotComputation
 import it.unibo.model.gameboard.GameBoardConfig.BotBehaviour.Balanced
 import it.unibo.model.gameboard.grid.ConcreteToken.Fire
-import it.unibo.model.gameboard.{GameBoard, GameBoardConfig}
+import it.unibo.model.gameboard.{ GameBoard, GameBoardConfig }
 import it.unibo.model.gameboard.grid.Position
-import it.unibo.model.gameboard.player.{Person, Player}
-import it.unibo.model.prolog.decisionmaking.{AttackDefense, DecisionMaker}
+import it.unibo.model.gameboard.player.{ Person, Player }
+import it.unibo.model.prolog.decisionmaking.{ AttackDefense, DecisionMaker }
 import org.scalatest.wordspec.AnyWordSpec
 import org.scalatest.BeforeAndAfterAll
 import org.scalatest.matchers.should.Matchers
@@ -27,17 +27,20 @@ class PatternChoiceSpec
   var gameBoard: GameBoard = uninitialized
 
   override def beforeAll(): Unit =
-    gameBoard = initialiseGameBoard(Person("Player1", List.empty, List.empty), Player.bot(Balanced))
-    val board = gameBoard.board.copy(grid = gameBoard.board.grid.setToken(Position(6, 7), Fire))
-    gameBoard = gameBoard.copy(board = board).changePlayer()
+    val player1 = Person("Player1", List.empty, List.empty)
+    val player2 = Player.bot(Balanced)
+    val gb      = GameBoard(player1, player2)
+    val newGrid = gb.board.grid.setToken(Position(6, 7), Fire)
+    val board   = gb.board.copy(grid = newGrid)
+    gameBoard = initialiseGameBoardPlayers(gb, player1, player2).copy(board = board).changePlayer()
 
   "A Pattern choice" should:
 
-    //doesnt' work
+    // doesnt' work
     "compute the right pattern to play" in:
       DecisionMaker.computeAttackOrDefense(gameBoard, Balanced)
       val direction        = gameBoard.board.windDirection
       val logicEffect      = WindEffect.windEffectSolver.solve(direction)
       val listLogicEffects = Map(1 -> List(logicEffect))
-      val a = computePatterns(gameBoard, listLogicEffects)
+      val a                = computePatterns(gameBoard, listLogicEffects)
       1 shouldBe 1
