2ce66c9dda2e98a995263a80f977990de8b54848
Riccardo Battistini <riccardo.battistini@protonmail.com>
2024-09-18 18:29:09 +0200

    refactor: use the IGameCard trait

--- src/main/scala/it/unibo/model/card/Card.scala
+++ src/main/scala/it/unibo/model/card/Card.scala
@@ -2,7 +2,7 @@ package it.unibo.model.card
 
 import it.unibo.model.effect.core.ICardEffect
 
-final case class Card(id: Int, title: String, description: String, effect: ICardEffect)
+final case class Card(id: Int, title: String, description: String, effect: ICardEffect) extends IGameCard
 
 object Card:
   def apply(cardId: Int, effect: ICardEffect) = new Card(cardId, "", "", effect)
