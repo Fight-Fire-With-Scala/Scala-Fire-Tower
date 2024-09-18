cbc340eaff2aef68becd5507b9b02d6c83e0ef9a
Alessandro Becci <alessandro.becci@studio.unibo.it>
2024-09-18 14:24:14 +0200

    refactor: removed useless GridManager.scala

--- src/main/scala/it/unibo/view/component/game/gameboard/grid/GridComponent.scala
+++ src/main/scala/it/unibo/view/component/game/gameboard/grid/GridComponent.scala
@@ -1,18 +1,26 @@
 package it.unibo.view.component.game.gameboard.grid
 
+import scala.collection.mutable
 import scala.compiletime.uninitialized
 
 import it.unibo.controller.ViewSubject
+import it.unibo.launcher.Launcher.view.runOnUIThread
 import it.unibo.model.gameboard.GamePhase
+import it.unibo.model.gameboard.grid.Cell.EternalFire
+import it.unibo.model.gameboard.grid.Cell.Tower
+import it.unibo.model.gameboard.grid.Cell.Woods
 import it.unibo.model.gameboard.grid.Grid
 import it.unibo.model.gameboard.grid.Position
 import it.unibo.model.gameboard.grid.Token
 import it.unibo.view.GUIType
 import it.unibo.view.component.IGridComponent
 import it.unibo.view.component.IUpdateView
+import it.unibo.view.component.game.gameboard.grid.handler.GridEventHandler
 import javafx.fxml.FXML
 import javafx.scene.Node
 import javafx.scene.layout.StackPane
+import scalafx.scene.layout.GridPane
+import scalafx.scene.paint.Color
 
 final class GridComponent(using
     observableSubject: ViewSubject
@@ -25,26 +33,59 @@ final class GridComponent(using
   private val cellSize   = 42
 
   @FXML
-  private var container: StackPane     = uninitialized
-  private var gridManager: GridManager = uninitialized
+  private var container: StackPane = uninitialized
+
+  private var gamePhase: GamePhase                 = uninitialized
+  var squareMap: mutable.Map[Position, GridSquare] = mutable.Map()
+  private var gridEventHandler: GridEventHandler   = uninitialized
 
   @FXML
   def initialize(): Unit =
-    gridManager = new GridManager(cellNumber, cellSize, observableSubject)
-    gridManager.initialize(container)
+    val gridInitializer = new GridInitializer(
+      cellNumber,
+      cellSize,
+      handleCellHover,
+      handleCellClick
+    )
+    val gridPane = new GridPane
+    squareMap = gridInitializer.initializeGridSquares(gridPane)
+    gridEventHandler = new GridEventHandler(observableSubject, squareMap)
+    container.getChildren.add(gridPane)
 
-  def setAvailablePatterns(patterns: Set[Map[Position, Token]], cardEffect: Int): Unit = gridManager
-    .setAvailablePatterns(patterns, cardEffect)
+  private def handleCellClick(row: Int, col: Int): Unit =
+    gridEventHandler.handleCellClick(row, col, gamePhase)
 
-  override def onEnableView(): Unit = gridManager.squareMap.foreach { case (_, square) =>
+  private def handleCellHover(row: Int, col: Int, hoverDirection: HoverDirection): Unit =
+    gridEventHandler.handleCellHover(row, col, hoverDirection, gamePhase)
+
+  def setAvailablePatterns(patterns: Set[Map[Position, Token]], cardEffect: Int): Unit =
+    gridEventHandler.updateAvailablePatterns(patterns)
+    gridEventHandler.setEffectCode(cardEffect)
+
+  def updateGrid(grid: Grid, currentTowerPositions: Set[Position], gamePhase: GamePhase): Unit =
+    squareMap.foreach { case (position, square) =>
+      this.gamePhase = gamePhase
+      val cellColor = grid.getCell(position) match
+        case Some(_: Woods.type) => Color.DarkGreen
+        case Some(_: Tower.type) if !currentTowerPositions.contains(position) =>
+          Color.rgb(131, 18, 22)
+        case Some(_: Tower.type)       => Color.rgb(76, 39, 3)
+        case Some(_: EternalFire.type) => Color.Red
+        case _                         => Color.White
+
+      val tokenColor = grid.getToken(position) match
+        case Some(token: Token) => token.color
+        case _                  => cellColor
+
+      runOnUIThread(square.updateColor(tokenColor))
+    }
+
+  override def onEnableView(): Unit = squareMap.foreach { case (_, square) =>
     square.enableView()
   }
 
-  override def onDisableView(): Unit = gridManager.squareMap.foreach { case (_, square) =>
+  override def onDisableView(): Unit = squareMap.foreach { case (_, square) =>
     square.disableView()
   }
 
   override protected def getPane: Node = container
-
-  def updateGrid(grid: Grid, currentTowerPositions: Set[Position], gamePhase: GamePhase): Unit =
-    gridManager.updateGrid(grid, currentTowerPositions, gamePhase)
